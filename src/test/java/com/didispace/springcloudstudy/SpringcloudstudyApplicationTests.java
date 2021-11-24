d217c21547ca14e148d943fb87e95c35f2bf6985
lb1565387341 <1565387341@qq.com>
2021-11-24 21:42:42 +0800

    git init

---
+++ src/test/java/com/didispace/springcloudstudy/SpringcloudstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didispace.springcloudstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
